fbcef4c238b620aafdeb850c290e72960e049970
dustyfingers <dustyfingers01257@gmail.com>
2019-09-11 15:54:59 -0600

    added more stuff

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const carsRouter = require('./cars.js');
 
 const db = require('./data/dbConfig.js');
 
 const server = express();
 
 server.use(express.json());
+server.use('/cars', carsRouter);
 
 module.exports = server;
