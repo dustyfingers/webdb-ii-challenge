6098c127c67964d19c75a26b52d1aebf57339eed
dustyfingers <dustyfingers01257@gmail.com>
2019-09-11 15:45:03 -0600

    aded db, knex and migrations files

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const accountsRouter = require('./accountsRouter.js');
+
+const db = require('./data/dbConfig.js');
 
 const server = express();
 
 server.use(express.json());
-server.use('/accounts', accountsRouter);
 
 module.exports = server;
